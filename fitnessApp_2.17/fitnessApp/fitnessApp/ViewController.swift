9c1af11d2a8fb8735f94336bbbbead4540b79d90
sophiepavia <68610925+sophiepavia@users.noreply.github.com>
2021-02-18 09:52:59 -0500

    Add files via upload
    I added two story boards to serve as the main app page and the profile page

---
+++ fitnessApp_2.17/fitnessApp/fitnessApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fitnessApp
+//
+//  Created by Sophie Pavia on 2/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
