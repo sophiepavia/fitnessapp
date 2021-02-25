9db49c371384806cf0a1a119cf854cfff3361ef7
sophiepavia <68610925+sophiepavia@users.noreply.github.com>
2021-02-25 09:10:21 -0500

    Add files via upload
    fixed embedded files and opening error

---
+++ fitApp/fitnessApp/MainPageViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainPageViewController.swift
+//  fitnessApp
+//
+//  Created by Sophie Pavia on 2/18/21.
+//
+
+import UIKit
+
+class MainPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ fitApp/fitnessApp/Step3ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  Step3ViewController.swift
+//  fitnessApp
+//
+//  Created by Sydney m on 2/16/21.
+//
+
+import UIKit
+
+class Step3ViewController: UIViewController {
+
+    //MARK: Properties
+    
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var weightText: UITextField!
+    @IBOutlet weak var heightText: UITextField!
+   
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+    }
+
+}
