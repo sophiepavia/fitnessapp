5930acffaa5ceda8e1c4b06ac8a6a514482e6791
alexserrano19 <serranoalex372@gmail.com>
2021-03-10 20:39:48 -0500

    Improved workout structure

---
+++ fitApp/fitnessApp/CreateNewExercise.swift
@@ -0,0 +1,44 @@
+//
+//  CreateNewExercise.swift
+//  fitnessApp
+//
+//  Created by Alex Serrano on 3/10/21.
+//
+
+import UIKit
+
+class CreateNewExercise: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func createNewExerciseButton(_ sender: Any) {
+        showAlert()
+    }
+    
+    func showAlert()
+    {
+        let alert = UIAlertController(title: "Notification", message: "This exercise has been succesfully added to your workout!", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Done", style: .default, handler: {action in
+            print("tapped Done")
+        }))
+        
+        present(alert, animated: true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
