30e246336e71157bee71bc48ed7d63247ba9fe0d
henrygilbert22 <77060624+henrygilbert22@users.noreply.github.com>
2021-03-25 10:49:00 -0400

    Create Service.php

---
+++ API/Service.php
@@ -0,0 +1,42 @@
+<?php
+ 
+ const DBNAME = "cfaoptim_FitnessApp";
+ const USERNAME = "cfaoptim_group";
+ const PASS = "password";
+// Create connection
+$con=mysqli_connect("localhost",USERNAME,PASS,DBNAME);
+ 
+// Check connection
+if (mysqli_connect_errno())
+{
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+echo "Were in boy";
+ 
+// This SQL statement selects ALL from the table 'Locations'
+$sql = "SELECT * FROM UserInfo";
+ 
+// Check if there are results
+if ($result = mysqli_query($con, $sql))
+{
+	// If so, then create a results array and a temporary one
+	// to hold the data
+	$resultArray = array();
+	$tempArray = array();
+ 
+	// Loop through each row in the result set
+	while($row = $result->fetch_object())
+	{
+		// Add each row into our results array
+		$tempArray = $row;
+	    array_push($resultArray, $tempArray);
+	}
+ 
+	// Finally, encode the array to JSON and output the results
+	echo json_encode($resultArray);
+}
+ 
+// Close connections
+mysqli_close($con);
+?>
