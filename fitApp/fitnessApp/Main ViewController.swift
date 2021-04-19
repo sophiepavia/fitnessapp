f989eb3ab6d60963437a044d1538e97d60abc9aa
pepperdog80 <sem18@my.fsu.edu>
2021-04-19 15:38:42 -0400

    Added missing Main ViewController file

---
+++ fitApp/fitnessApp/Main ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  Main ViewController.swift
+//  fitnessApp
+//
+//  Created by Sydney m on 4/19/21.
+//
+
+import UIKit
+
+class Main_ViewController: UIViewController {
+
+    
+    func assignbackground(){
+          let background = UIImage(named: "Hexagon background")
+          var imageView : UIImageView!
+          imageView = UIImageView(frame: view.bounds)
+        imageView.contentMode =  UIView.ContentMode.scaleAspectFill
+          imageView.clipsToBounds = true
+          imageView.image = background
+          imageView.center = view.center
+          view.addSubview(imageView)
+          self.view.sendSubviewToBack(imageView)
+      }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        assignbackground()
+    }
+
+}
